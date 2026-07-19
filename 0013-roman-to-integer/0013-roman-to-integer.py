918c476a3606d03d206a2bc75fe1c3c20189737b
Nirvan <166929974+jadoo12343@users.noreply.github.com>
2026-07-19 23:56:14 +0530

    Time: 6 ms (46.19%), Space: 19.2 MB (57.37%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -2,24 +2,12 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         values={'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
         N=len(s)
-        index=0
         result=0
-        while index < N:
-            char=s[index]
-            value=values[char]
-            if index+1<N:
-                nex_char=s[index+1]
-                nex_value=values[nex_char]
-                if value<nex_value:
-                    result+=(nex_value-value)
-                    index+=1
-                else:
-                    result+=value
+        for i in range(N):
+            if i+1<N and values[s[i]]<values[s[i+1]]:
+                result-=values[s[i]]
             else:
-                result+=value
-                index+=1
-            index+=1
-
+                result+=values[s[i]]
         return result
             
 
