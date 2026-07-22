f4221b8beed1898e71f288915fdde0675fd96f0f
Nirvan <166929974+jadoo12343@users.noreply.github.com>
2026-07-22 22:48:08 +0530

    Time: 0 ms (100%), Space: 19.3 MB (22.71%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,8 +1,6 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        n=''.join(sorted(s))
-        m=''.join(sorted(t))
-        for i in m:
+        for i in t:
             if s.count(i)!=t.count(i):
                 return i
         
