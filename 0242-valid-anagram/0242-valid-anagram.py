f678396bf590cd2a50b15e5144afa4633c2b42c2
Nirvan <166929974+jadoo12343@users.noreply.github.com>
2026-07-23 22:18:36 +0530

    Time: 18 ms (19.49%), Space: 20 MB (22.91%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,13 +1,10 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        n=len(s)
-        m=len(t)
-        sum1=0
-        if n==m:
-            for i in s :
-                if s.count(i)==t.count(i):
-                    sum1+=1
-        return sum1==len(s)
+        k=''.join(sorted(s))
+        l=''.join(sorted(t))
+        return k==l
+
+
         
 
                 
