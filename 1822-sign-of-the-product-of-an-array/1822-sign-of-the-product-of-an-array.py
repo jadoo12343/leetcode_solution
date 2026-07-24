0f45a891bab333d1988ca3696a30c1d4f5258920
Nirvan <166929974+jadoo12343@users.noreply.github.com>
2026-07-24 23:35:08 +0530

    Time: 0 ms (100%), Space: 19.3 MB (71.94%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
@@ -1,12 +1,10 @@
 class Solution:
     def arraySign(self, nums: List[int]) -> int:
-        num=1
-        for i in nums:
-            num*=i
-        if num > 0:
-            return 1
-        elif num<0:
-            return -1
-        else:
-            return 0
-
+        si = 1
+        for num in nums:
+            if num == 0:
+                return 0
+            if num < 0:
+                si = -si
+                
+        return si        
