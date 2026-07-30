dc445653fa669d2363b0c34e266c905992d2cb91
Nirvan <166929974+jadoo12343@users.noreply.github.com>
2026-07-30 22:56:45 +0530

    Time: 0 ms (100%), Space: 19.2 MB (97.36%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,9 +1,5 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        a_int, b_int = int(a, 2), int(b, 2)
-        while b_int != 0:
-            carry = a_int & b_int
-            a_int = a_int ^ b_int
-            b_int = carry << 1
-        return bin(a_int)[2:]
+        c = bin(int(a,2) + int(b,2))
+        return c[2:]
 
