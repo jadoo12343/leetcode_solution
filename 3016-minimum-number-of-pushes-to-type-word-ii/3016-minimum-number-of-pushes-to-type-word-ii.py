fa2e0bf56b214bc420511b70b7372b177db937f3
Nirvan <166929974+jadoo12343@users.noreply.github.com>
2026-08-01 23:37:27 +0530

    Time: 175 ms (35.31%), Space: 19.9 MB (94.34%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        freq = [0] * 26
+
+        for ch in word:
+            freq[ord(ch) - ord('a')] += 1
+
+        freq.sort()
+
+        minPushing = 0
+        pushCnt = 0
+
+        for i in range(25, -1, -1):
+            currEleIdx = 25 - i
+
+            if currEleIdx % 8 == 0:
+                pushCnt += 1
+
+            minPushing += freq[i] * pushCnt
+
+        return minPushing
+        
