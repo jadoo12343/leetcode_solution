a6a79c857ec38203af1e728e04e70bec82e0add0
Nirvan <166929974+jadoo12343@users.noreply.github.com>
2026-08-25 14:15:49 +0530

    Time: 0 ms (100%), Space: 19.4 MB (8.83%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,20 +1,8 @@
-class Solution:
-    def arrangeCoins(self, n: int) -> int:
-        ans = 0
-        l = 1
-        h = n//2
-        if n == 1:
-            return 1
-        if n == 3:
-            return 2
-        while l <= h:
-            mid = (l+h)//2
-            if (mid*(mid+1))//2 <= n:
-                ans = mid
-                l = mid+1
-            else:
-                h = mid-1
-        return ans
+import math
+
+class Solution(object):
+    def arrangeCoins(self, n):
+        return int((math.sqrt(8 * n + 1) - 1) // 2)
 
         
         
