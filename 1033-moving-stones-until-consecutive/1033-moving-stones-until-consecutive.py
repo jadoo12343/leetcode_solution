d958f48f7559f3a5d4722bcf8a7df8e3256dfd45
Nirvan <166929974+jadoo12343@users.noreply.github.com>
2026-08-31 09:37:59 +0530

    Time: 0 ms (100%), Space: 19.4 MB (26.88%) - LeetHub

--- 1033-moving-stones-until-consecutive/1033-moving-stones-until-consecutive.py
+++ 1033-moving-stones-until-consecutive/1033-moving-stones-until-consecutive.py
@@ -1,7 +1,8 @@
 class Solution:
-    def numMovesStones(self, a, b, c):
+    def numMovesStones(self, a: int, b: int, c: int) -> List[int]:
         maxi = max(a,b,c) - min(a,b,c) - 2
         mini = max((min(abs(a-b),abs(b-c),abs(a-c), 3) - 1), 1)
-        if maxi == 0: mini = 0            
+        if maxi == 0: 
+            mini = 0            
         return [mini,maxi]
         
