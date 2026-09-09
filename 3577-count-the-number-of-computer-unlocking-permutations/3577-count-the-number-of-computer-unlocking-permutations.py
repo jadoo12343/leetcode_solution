6a614e7482f1b5e83803f4404ca85e96b48f4aad
Nirvan <166929974+jadoo12343@users.noreply.github.com>
2026-09-09 12:33:24 +0530

    Time: 19 ms (72.62%), Space: 32.7 MB (40.48%) - LeetHub

--- 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.py
+++ 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.py
@@ -1,16 +1,12 @@
 class Solution:
     MOD = 10**9 + 7
-
     def countPermutations(self, complexity: List[int]) -> int:
         n = len(complexity)
         first = complexity[0]
-
         for i in range(1, n):
             if complexity[i] <= first:
                 return 0
-
-        fact = 1
+        res = 1
         for i in range(2, n):
-            fact = (fact * i) % self.MOD
-
-        return fact
+            res = (res * i) % self.MOD
+        return res
