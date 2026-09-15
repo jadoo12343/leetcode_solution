42239f870dd3f03e038a8a34c8cfe688f37712f5
Nirvan <166929974+jadoo12343@users.noreply.github.com>
2026-09-15 13:33:12 +0530

    Time: 3 ms (65.14%), Space: 19.4 MB (42.27%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,24 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        dummyHead = ListNode(0)
-        tail = dummyHead
-        carry = 0
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        res = None
+        head = None
+        change = 0
+        while l1 or l2:
+            if l1:
+                val1 = l1.val
+                l1 = l1.next
+            else:
+                val1 = 0
 
-        while l1 is not None or l2 is not None or carry != 0:
-            digit1 = l1.val if l1 is not None else 0
-            digit2 = l2.val if l2 is not None else 0
+            if l2:
+                val2 = l2.val
+                l2 = l2.next
+            else:
+                val2 = 0
 
-            sum = digit1 + digit2 + carry
-            digit = sum % 10
-            carry = sum // 10
-
-            newNode = ListNode(digit)
-            tail.next = newNode
-            tail = tail.next
-
-            l1 = l1.next if l1 is not None else None
-            l2 = l2.next if l2 is not None else None
-
-        result = dummyHead.next
-        dummyHead.next = None
-        return result
+            summ = val1 + val2 + change
+            if not res:
+                head = res = ListNode(summ % 10)
+            else:
+                res.next = ListNode(summ % 10)
+                res = res.next
+            
+            change = summ // 10
+        if change:
+            res.next = ListNode(change)
+        return head
