ce3b519dbd75cc38b539bb76ebe24cd00c9da44b
Nirvan <166929974+jadoo12343@users.noreply.github.com>
2026-09-15 13:14:43 +0530

    Time: 7 ms (95.28%), Space: 19 MB (98.82%) - LeetHub

--- 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.py
+++ 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.py
@@ -1,19 +1,26 @@
 class Solution:
-    def minPair(self, v: List[int]) -> int:
-        min_sum = 10**9
-        pos = -1
-        for i in range(len(v) - 1):
-            if v[i] + v[i + 1] < min_sum:
-                min_sum = v[i] + v[i + 1]
-                pos = i
-        return pos
-    def mergePair(self, v: List[int], pos: int) -> None:
-        v[pos] += v[pos + 1]
-        del v[pos + 1]
-
     def minimumPairRemoval(self, nums: List[int]) -> int:
-        ops = 0
-        while nums != sorted(nums): 
-            self.mergePair(nums, self.minPair(nums))
-            ops += 1
-        return ops
+        def isSorted(nums, n) -> bool:
+            for i in range(1,n):
+                if nums[i] < nums[i - 1]:
+                    return False
+            return True
+        
+        ans, n = 0, len(nums)
+        
+        while not isSorted(nums, n):
+            ans += 1
+            min_sum, pos = float('inf'), -1
+            for i in range(1,n):
+                sum = nums[i - 1] + nums[i]
+                if sum < min_sum:
+                    min_sum = sum
+                    pos = i
+            nums[pos - 1] = min_sum
+            for i in range(pos, n-1): 
+                nums[i] = nums[i + 1]
+            n -= 1
+        
+        return ans
+        
+        
