97d023f139c25127538217adc1feb7821a52d1ed
Nirvan <166929974+jadoo12343@users.noreply.github.com>
2026-09-17 12:20:38 +0530

    Time: 3 ms (98.56%), Space: 21.4 MB (80.44%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.py
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.py
@@ -11,29 +11,18 @@
 #         self.right = right
 class Solution:
     def sortedListToBST(self, head: ListNode) -> TreeNode:
-        if not head:
+        if not head :
             return None
-        if not head.next:
+        elif not head.next:
             return TreeNode(head.val)
-
-        slow, fast = head, head
+        slow , fast = head , head
         slow_prev = None
-
-        # Find the middle node (slow pointer)
-        while fast and fast.next:
+        while fast and fast.next :
             slow_prev = slow
             slow = slow.next
             fast = fast.next.next
-
-        # Create root node from the middle element
         root = TreeNode(slow.val)
-
-        # Disconnect the left half from the middle
         slow_prev.next = None
-
-        # Recursively construct left and right subtrees
         root.left = self.sortedListToBST(head)
         root.right = self.sortedListToBST(slow.next)
-
         return root
-        
